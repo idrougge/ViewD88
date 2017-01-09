38727ac4554802241a6574ed525930c140eb6d48
Iggy Drougge <idrougge@mac.com>
2017-01-09 17:01:51 +0100

    Initial Commit

---
+++ ViewD88/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViewD88
+//
+//  Created by Iggy Drougge on 2017-01-09.
+//  Copyright © 2017 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
