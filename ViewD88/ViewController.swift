bbb8057ffa0b12979acecac4ea6e6d5efbecb054
Iggy Drougge <idrougge@mac.com>
2017-01-10 11:28:01 +0100

    Save raw image of disk

--- ViewD88/ViewController.swift
+++ ViewD88/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     @IBOutlet var dumpFileButton: NSButton!
     var diskimage:D88Image!
     var files:[D88Image.FileEntry]!
+    var fileurl:URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textField.isEditable = false
         let imgpath = URL(fileURLWithPath: NSHomeDirectory()+"/Documents/d88-swift/basic.d88")
+        fileurl = imgpath
         textField.stringValue = imgpath.lastPathComponent
         guard let imgdata = try? Data(contentsOf: imgpath) else { fatalError("Fel vid inläsning av fil")}
         diskimage = D88Image(data: imgdata)
         files = diskimage.getFiles()
+        
         table.dataSource = self
         table.delegate = self
         table.allowsMultipleSelection = true
@@ -40,7 +43,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         return files.count
     }
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        print(#function,tableColumn!.identifier)
+        //print(#function,tableColumn!.identifier)
         let cell = tableView.make(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
         switch tableColumn?.identifier {
         case let id where id == "nr": cell.textField?.stringValue = "nr \(row)"
@@ -81,7 +84,21 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         
     }
     @IBAction func didPressDumpContents(_ sender: Any) {
-        print(#function)
+        guard let fileurl = fileurl else {return}
+        let filename = fileurl.deletingPathExtension().appendingPathExtension("2d").lastPathComponent
+        print(#function,filename)
+        let savepanel = NSSavePanel()
+        savepanel.title = "Save raw contents of disk"
+        savepanel.nameFieldStringValue = filename
+        guard savepanel.runModal() == NSModalResponseOK,
+            let url = savepanel.url else { return }
+        let data = diskimage.rawData
+        do {
+            try data.write(to: url)
+        }
+        catch {
+            print(error)
+        }
     }
     @IBAction func didPressDumpFile(_ sender: Any) {
         guard table.selectedRow != -1 else {
@@ -91,17 +108,21 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         let file = files[table.selectedRow]
         print(#function, file.name)
         let dialogue = NSSavePanel()
-        dialogue.message = "Spara här"
-        dialogue.title = "Titel"
+        //dialogue.message = "Spara här"
+        dialogue.title = "Save file from disk image"
         dialogue.nameFieldStringValue = file.name
-        guard dialogue.runModal() == NSModalResponseOK, let url = dialogue.url else {
+        guard
+            dialogue.runModal() == NSModalResponseOK,
+            let url = dialogue.url
+            else {
             return
         }
         let filedata = diskimage.getFile(file: file)
         do {
             try filedata.write(to: url)
             print("Sparade fil på \(url)")
-        } catch {
+        }
+        catch {
             print(error)
         }
         
